0ce1205e5130a6d05644fe2d5093fbf21fc9146a
Charles Bugarin <36612532+caqb18@users.noreply.github.com>
2018-02-19 03:11:06 +0000

    Add files via upload

--- Iphone.py
+++ Iphone.py
@@ -8,11 +8,15 @@ import sys
 iphone = 50000
 salary = 0
 qty = 0
+
+print ("Reminder: An iphone costs P50000\n")
 salary = input ("How much is your money? ")
 qty = salary/iphone
-mmoney = iphone - salary
+mmoney = iphone - salary%iphone
 
 if qty < 1:
-    print ("You need P", mmoney, "to buy an Iphone")
+    print "You need P", mmoney, "to buy an Iphone"
+    
 else:
-    print ("You can buy", qty, "Iphone/s") 
+    print "You can buy", qty, "Iphone/s"
+    print "To buy another iphone you need P", mmoney
